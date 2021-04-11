aab5dc67ac618989d6879cd3ef7672eb332a3eb3
Rita Burjan <ritaburjan@gmail.com>
2021-04-11 14:46:23 -0500

    Teacher Added

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
 
 # Register your models here.
 @admin.register(School)
 class School(admin.ModelAdmin):
     list_display = ('name', 'abbreviation', 'address')
     list_editable = ('abbreviation', 'address')
+
+class TeacherInline(admin.StackedInline):
+    model = Teacher
+    can_delete = True
+
+class UserAdmin(AuthUserAdmin):
+    inlines = [TeacherInline]
+    list_display = ('id','username', 'first_name', 'last_name')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
