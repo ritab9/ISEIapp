f00e7e034aca9b47e1cb540fddeee735ddbdc6e6
Rita Burjan <ritaburjan@gmail.com>
2021-04-11 14:13:52 -0500

    School Added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(School)
+class School(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation', 'address')
+    list_editable = ('abbreviation', 'address')
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=50, help_text='Enter the name of the school', unique=True, blank=False,
+                            null=False)
+    address = models.CharField(max_length=50, help_text='Enter the name of the school', blank=True, null=True)
+    abbreviation = models.CharField(max_length=4, default=" ", help_text=' Enter the abbreviation for this school')
+    ordering = ['name']
+
+    def __str__(self):
+        return self.name
