3ae31a854ea5cee89418ce3d8cd22129d1ef167d
Rita Burjan <ritaburjan@gmail.com>
2021-05-09 14:40:03 -0500

    commented out all AWS references from settings.py

--- ISEIapp/settings.py
+++ ISEIapp/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 
 #AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 #AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
+#    'CacheControl': 'max-age=86400',
+#}
 #AWS_LOCATION = 'static'
 
 #STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
