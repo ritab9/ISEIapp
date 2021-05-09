4817c8f7c4b967535ae57b39ef1b5d8cd7f83816
Rita Burjan <ritaburjan@gmail.com>
2021-05-09 14:28:21 -0500

    Added False default for Debug

--- ISEIapp/settings.py
+++ ISEIapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False')
 
 # DJANGO_ALLOWED_HOSTS for production
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
