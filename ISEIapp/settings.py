e72a4f145e050fc153d6db181e20357d06d96c07
ritab9 <79879349+ritab9@users.noreply.github.com>
2021-06-25 09:20:51 +0300

    Trying out AWS again

--- ISEIapp/settings.py
+++ ISEIapp/settings.py
@@ -137,7 +137,6 @@ USE_TZ = True
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static' ),
 
 AWS_DEFAULT_ACL = None
-
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
@@ -145,10 +144,12 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'ISEIapp.storage_backends.MediaStorage'
+
+
+#AWS_LOCATION = 'static'
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'ISEIapp.storage_backends.MediaStorage'
 
 
 STATIC_URL = '/static/'
