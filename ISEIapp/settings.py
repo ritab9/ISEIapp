c6762ce020e9cf28aeb76c02c51a8c4765f5da0d
ritab9 <79879349+ritab9@users.noreply.github.com>
2021-11-03 14:07:16 -0500

    Printing settings on teacher_dashboard.html

--- ISEIapp/settings.py
+++ ISEIapp/settings.py
@@ -177,6 +177,8 @@ LOGOUT_REDIRECT_URL = '/login/'
 #the password is an app password (found in Account / Security)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST = 'mail.isei.life'
+#EMAIL_PORT = 465
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
