f319ac26bfc920ce9eba09b8cb1c4086a9bf95cc
ritab <ritab@iseiea.org>
2024-05-13 20:10:34 +0300

    change redirect for principal to teachercert

--- users/views.py
+++ users/views.py
@@ -60,8 +60,8 @@ def loginpage(request):
             else:
                 if request.user.is_active:
                     if is_in_group(request.user, 'principal'):
-                        #return redirect('principal_teachercert', user.id)
-                        return redirect('principal_dashboard', user.id)
+                        return redirect('principal_teachercert', user.id)
+                        #return redirect('principal_dashboard', user.id)
                     elif is_in_group(request.user, 'teacher'):
                             return redirect('teacher_dashboard', user.id)
                     elif is_in_group(request.user, 'staff'):
